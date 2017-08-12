e410eed8afddd5d96a8d04148b96319f6987c18d
afeiship <1290657123@qq.com>
2017-08-12 09:50:02 +0800

    optimize is array like

--- src/next-to-array.js
+++ src/next-to-array.js
@@ -2,10 +2,13 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
+  var isArrayLike = function(inObj){
+    typeof inObj.length == 'number'
+  };
 
   nx.toArray = function (inObj) {
     if (!inObj) return [];
-    if (nx.isArrayLike(inObj)) return slice.call(inObj);
+    if (isArrayLike(inObj)) return slice.call(inObj);
     return [inObj];
   };
 
