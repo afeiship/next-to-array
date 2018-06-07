c4a45ba1b7e8212ab4b43b26d6c9783faf7a2a4e
afeiship <1290657123@qq.com>
2018-06-07 12:33:14 +0800

    issue: bugfix

--- dist/next-to-array.js
+++ dist/next-to-array.js
@@ -3,14 +3,15 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var NUMBER = 'number';
+  var STRING = 'string';
   var isArrayLike = function (inObj) {
-    return typeof inObj.length === NUMBER;
+    return typeof inObj.length === NUMBER && typeof inObj !== STRING;
   };
 
   nx.toArray = function (inObj) {
     if (!inObj) return [];
     if (isArrayLike(inObj)) return nx.slice(inObj);
-    return [ inObj ];
+    return [inObj];
   };
 
   if (typeof module !== 'undefined' && module.exports) {
